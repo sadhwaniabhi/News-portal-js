6eeaa00c287d877360b747cbe79e3f2590d60ae2
Abhishek Sadhwani <abhisheksadhwani23@gmail.com>
2024-03-11 23:36:28 +0530

    removed my API key

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const cards = document.querySelector(".cards");
 const category = document.querySelector(".category");
 const categorySpans  = document.querySelectorAll(".category span");
 const baseUrl = "https://newsapi.org/v2";
-const api = "&apiKey=e293bc4d57b5411da5cb4ab83ec315dd";
+const api = "XXXXXXX";
 
 const backUpImage = "https://images.unsplash.com/photo-1504711434969-e33886168f5c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
 async function getRequestedData(url){
     try{
-        const response = await fetch(baseUrl + url + api);
-        const finalData = await response.json();    
+        const response = await fetch(baseUrl + url +"&apiKey="+ api);
+        const finalData = await response.json();  
         return finalData;
     }catch(error){
         console.log(error);
